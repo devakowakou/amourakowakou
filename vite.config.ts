7bc69028678641ede3d4f6d52a8d60b99b731a20
devakowakou <akowakou251@gmail.com>
2026-02-26 14:01:57 +0100

    fix: update base path in Vite config for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
-  base: '/devakowakou.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
